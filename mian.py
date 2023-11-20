000c2380c7f2e10cf68bb8febb916d5c32e00dc7
22046-NelsonEscalante <esc22046@uvg.edu.gt>
2023-11-20 09:34:26 -0600

    Layout for the functions

--- mian.py
+++ mian.py
@@ -1,1 +1,14 @@
-print("hello world")
+def mcd(a, b):
+    c = a % b
+    if (c != 0):
+        return mcd(b, c)
+    return b
+
+def Encode(p, q, e):
+    n = p*q
+    f = (p-1)*(q-1)
+
+    return 0
+
+def Decode():
+    return 0
