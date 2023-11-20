5bf2984e0637cbacc3f8efabcb8963a906d1dd5f
oscarfuentes22 <fue22763@uvg.edu.gt>
2023-11-20 09:48:10 -0600

    Verificar si son primos

--- mian.py
+++ mian.py
@@ -5,6 +5,7 @@ def mcd(a, b):
     return b
 
 def Encode(p, q, e):
+    
     n = p*q
     f = (p-1)*(q-1)
 
@@ -12,3 +13,28 @@ def Encode(p, q, e):
 
 def Decode():
     return 0
+def es_primo(numero):
+    if numero <= 1:
+        return False
+    if numero <= 3:
+        return True
+    if numero % 2 == 0 or numero % 3 == 0:
+        return False
+    i = 5
+    while i * i <= numero:
+        if numero % i == 0 or numero % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+def verificar_primos(p, q):
+    if es_primo(p) and es_primo(q):
+        print(f"{p} y {q} son números primos.")
+    else:
+        print(f"{p} y {q} no son números primos.")
+
+# Aquí puedes ingresar los valores de p y q que desees verificar
+p = int(input("Ingrese el valor de p: "))
+q = int(input("Ingrese el valor de q: "))
+
+
