448386352e164397af1b8c2f3edfc0e1fbce5c9d
oscarfuentes22 <fue22763@uvg.edu.gt>
2023-11-20 10:09:58 -0600

    funcion ecuacion diofantica

--- mian.py
+++ mian.py
@@ -34,7 +34,32 @@ def verificar_primos(p, q):
         print(f"{p} y {q} no son números primos.")
 
 # Aquí puedes ingresar los valores de p y q que desees verificar
-p = int(input("Ingrese el valor de p: "))
-q = int(input("Ingrese el valor de q: "))
+#p = int(input("Ingrese el valor de p: "))
+#q = int(input("Ingrese el valor de q: "))
+def euclides_extendido(a, b):
+    if a == 0:
+        return b, 0, 1
+    gcd, x1, y1 = euclides_extendido(b % a, a)
+    x = y1 - (b // a) * x1
+    y = x1
+    return gcd, x, y
+
+def resolver_diofantica(a, b):
+    gcd, x, y = euclides_extendido(a, b)
+
+    if 1 % gcd != 0:
+        return "No tiene solución"
+
+    x *= 1 // gcd
+    y *= 1 // gcd
+
+    return x, y
+
+# Ejemplo de uso
+a = 13
+b = 2436
+
+solucion = resolver_diofantica(a, b)
+print(f"Solución: x = {solucion[0]}, y = {solucion[1]}")
 
 
