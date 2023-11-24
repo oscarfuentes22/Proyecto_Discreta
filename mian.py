60b4a52c68e4893da01efda50a27b900285193b2
22046-NelsonEscalante <esc22046@uvg.edu.gt>
2023-11-23 22:01:53 -0600

    Reversed the dictionary and added a function to translate

--- mian.py
+++ mian.py
@@ -1,31 +1,38 @@
 alphabet = {
-    "A": 0,
-    "B": 1,
-    "C": 2,
-    "D": 3,
-    "E": 4,
-    "F": 5,
-    "G": 6,
-    "H": 7,
-    "I": 8,
-    "J": 9,
-    "K": 10,
-    "L": 11,
-    "M": 12,
-    "N": 13,
-    "O": 14,
-    "P": 15,
-    "Q": 16,
-    "R": 17,
-    "S": 18,
-    "T": 19,
-    "U": 20,
-    "V": 21,
-    "W": 22,
-    "X": 23,
-    "Y": 24,
-    "Z": 25,
+    0: 'A',
+    1: 'B', 
+    2: 'C', 
+    3: 'D', 
+    4: 'E', 
+    5: 'F', 
+    6: 'G', 
+    7: 'H', 
+    8: 'I', 
+    9: 'J', 
+    10: 'K', 
+    11: 'L', 
+    12: 'M', 
+    13: 'N', 
+    14: 'O', 
+    15: 'P', 
+    16: 'Q', 
+    17: 'R', 
+    18: 'S', 
+    19: 'T', 
+    20: 'U', 
+    21: 'V', 
+    22: 'W', 
+    23: 'X', 
+    24: 'Y', 
+    25: 'Z'
 }
+
+def Translate(m: int):
+    Msg = str(m)
+    if (len(Msg) == 3):
+        return 0
+    else:
+        return 1
 
 def mcd(a, b):
     c = a % b
@@ -50,13 +57,14 @@ def power(b, n, m):
 
 def Decode(m, e, p, q):
     n = p*q
-    print(n)
+    #print(n)
     f = (p-1)*(q-1)
-    print(f)
-    d, y = resolver_diofantica(e, f)
-    print(d)
+    #print(f)
+    d = resolver_diofantica(e, f)
+    #print(d)
 
-    print(power(m, d, n))
+    #print(power(m, d, n))
+    print(pow(m, d, f))
 
     return 0
 
@@ -100,15 +108,16 @@ def resolver_diofantica(a, b):
     x *= 1 // gcd
     y *= 1 // gcd
 
-    return x, y
+    return x
 
 # Ejemplo de uso
 a = 13
 b = 2436
 
-solucion = resolver_diofantica(a, b)
-print(f"Solución: x = {solucion[0]}, y = {solucion[1]}")
+#solucion = resolver_diofantica(a, b)
+#print(f"Solución: x = {solucion[0]}, y = {solucion[1]}")
 
 Decode(1366, 17, 53, 37)
+Decode(416, 13, 43, 59)
 
 
